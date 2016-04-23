6c9d53ebad3e0229d2f5fc8d85bf5f331c17f189
Beth Brewer <bethbrewer@Beths-MBP.attlocal.net>
2016-04-22 23:30:19 -0500

    Assignment-35-incomplete

---
+++ spec/models/topic_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+include RandomData
+
+  RSpec.describe Topic, type: :model do
+    let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+
+    describe "Topic" do
+    it { is_expected.to have_many(:posts) }
+
+    it { is_expected.to validate_length_of(:name).is_at_least(5) }
+    it { is_expected.to validate_length_of(:description).is_at_least(15) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:description) }
+  end
+
+
+    describe "attributes" do
+    it "should respond to name" do
+      expect(topic).to respond_to(:name)
+    end
+
+    it "should respond to description" do
+      expect(topic).to respond_to(:description)
+    end
+
+    it "should respond to public" do
+      expect(topic).to respond_to(:public)
+    end
+
+    it "should be public by default" do
+      expect(topic.public).to be(true)
+    end
+
+  end
+end
