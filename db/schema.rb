9f8db2e194cfdde11be469d63d6bd4c586f08581
Beth Brewer <bethbrewer@beths-mbp.attlocal.net>
2016-04-27 18:25:32 -0500

    Checkpoint 36 Complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417182023) do
+ActiveRecord::Schema.define(version: 20160427231751) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -38,23 +38,22 @@ ActiveRecord::Schema.define(version: 20160417182023) do
     t.integer  "topic_id"
   end
 
-   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
+  add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
 
-   create_table "questions", force: :cascade do |t|
-     t.string   "title"
-     t.text     "body"
-     t.boolean  "resolved"
-     t.datetime "created_at", null: false
-     t.datetime "updated_at", null: false
-   end
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
-   create_table "topics", force: :cascade do |t|
-     t.string   "name"
-     t.boolean  "public",      default: true
-     t.text     "description"
-     t.datetime "created_at",                 null: false
-     t.datetime "updated_at",                 null: false
-
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
