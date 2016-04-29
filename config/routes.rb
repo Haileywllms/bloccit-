977773da46f361981a00d577096c2416b6defdd6
Beth Brewer <bethbrewer@beths-mbp.attlocal.net>
2016-04-28 21:34:38 -0500

    Assignment 37 Signing Up

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
   resources :users, only: [:new, :create]
+  post 'users/confirm' => 'users#confirm'
+
   get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
-
-
 end
