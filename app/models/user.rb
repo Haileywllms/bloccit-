ee154e14d288ef2d13772dd32ea8dfd674d1f46c
Beth Brewer <bethbrewer@beths-mbp.attlocal.net>
2016-04-28 20:33:48 -0500

    Assignment 36 Complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  before_save :format_name
 
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
@@ -12,4 +13,14 @@ class User < ActiveRecord::Base
 
 
   has_secure_password
-end
+
+  def format_name
+    if self.name
+      name_array = []
+      self.name.split.each do |name_part|
+        name_array << name_part.capitalize
+      end
+      self.name = name_array.join(" ")
+    end
+  end
+end  
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,36 +2,54 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
-  # Shoulda tests for name
+
+  # shoulda tests for name
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
 
-  # Shoulda tests for email
+  # shoulda tests for email
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to validate_length_of(:email).is_at_least(3) }
   it { is_expected.to allow_value("user@bloccit.com").for(:email) }
 
-  # Shoulda tests for password
+  # shoulda tests for password
   it { is_expected.to validate_presence_of(:password) }
   it { is_expected.to have_secure_password }
   it { is_expected.to validate_length_of(:password).is_at_least(6) }
 
   describe "attributes" do
-    it "should have name and email attributes" do
-      expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
+    it "should respond to name" do
+      expect(user).to respond_to(:name)
+    end
+
+    it "should capitalize the name" do
+      user_with_lowercase_name = User.create!(name: "steve jobs", email: "stevejobs@bloccit.com", password: "password")
+
+      expect(user_with_lowercase_name.name).to eq "Steve Jobs"
+    end
+
+    it "should respond to email" do
+      expect(user).to respond_to(:email)
     end
   end
-  describe "invalid user" do
-     let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
-     let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
 
-     it "should be an invalid user due to blank name" do
-       expect(user_with_invalid_name).to_not be_valid
-     end
- 
-     it "should be an invalid user due to blank email" do
-       expect(user_with_invalid_email).to_not be_valid
-     end
-   end
+  describe "invalid user" do
+    let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
+    let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
+    let(:user_with_invalid_email_format) { User.new(name: "Bloccit User", email: "invalid_format") }
+
+    it "should be an invalid user due to blank name" do
+      expect(user_with_invalid_name).to_not be_valid
+    end
+
+    it "should be an invalid user due to blank emal" do
+      expect(user_with_invalid_email).to_not be_valid
+    end
+
+    it "should be an invalid user due to incorrectly formatted email address" do
+      expect(user_with_invalid_email_format).to_not be_valid
+    end
+  end
+
 end
