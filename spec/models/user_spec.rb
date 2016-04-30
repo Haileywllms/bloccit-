702cf8420b3ce5f77b9a9f9c57275b041621a52d
Beth Brewer <bethbrewer@beths-mbp.attlocal.net>
2016-04-30 14:17:09 -0500

    checkpoint 39 Complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+
+  it { is_expected.to have_many(:posts) }
   # Shoulda tests for name
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
@@ -29,7 +31,7 @@ RSpec.describe User, type: :model do
      it "should be an invalid user due to blank name" do
        expect(user_with_invalid_name).to_not be_valid
      end
- 
+
      it "should be an invalid user due to blank email" do
        expect(user_with_invalid_email).to_not be_valid
      end
