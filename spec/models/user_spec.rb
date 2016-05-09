84b0064fe1093437db32198787ac6484dc02cc02
Brittany Martin <brittany.jill.martin@gmail.com>
2016-05-08 20:27:11 -0400

    Seeding and Tests work

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User, type: :model do
   it { is_expected.to have_many(:posts) }
   it { is_expected.to have_many(:comments) }
   it { is_expected.to have_many(:votes) }
-  it { is_expected.to have_many(:favorites) }
+
   # Shoulda tests for name
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
@@ -23,27 +23,8 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_length_of(:password).is_at_least(6) }
 
   describe "attributes" do
-    it "should have name and email attributes" do
-      expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
-    end
-  end
-
-    it "responds to role" do
-      expect(user).to respond_to(:role)
-    end
-
-# #2
-    it "responds to admin?" do
-      expect(user).to respond_to(:admin?)
-    end
-
-    it "responds to moderator?" do
-      expect(user).to respond_to(:moderator?)
-    end
-
-# #3
-    it "responds to member?" do
-      expect(user).to respond_to(:member?)
+    it "has name and email attributes" do
+      expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com", role: "member", admin?: false, member?: true)
     end
   end
 
@@ -51,9 +32,7 @@ RSpec.describe User, type: :model do
     it "is member by default" do
       expect(user.role).to eq("member")
     end
-  end
 
-# #5
     context "member user" do
       it "returns true for #member?" do
         expect(user.member?).to be_truthy
@@ -62,13 +41,8 @@ RSpec.describe User, type: :model do
       it "returns false for #admin?" do
         expect(user.admin?).to be_falsey
       end
-
-      it "returns false for #moderator?" do
-        expect(user.moderator?).to be_falsey
-      end
     end
 
-# #6
     context "admin user" do
       before do
         user.admin!
@@ -82,49 +56,18 @@ RSpec.describe User, type: :model do
         expect(user.admin?).to be_truthy
       end
     end
+  end
 
-    context "moderator user" do
-      before do
-        user.moderator!
-      end
+  describe "invalid user" do
+    let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
+    let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
 
-      it "returns false for #member?" do
-        expect(user.member?).to be_falsey
-      end
-
-      it "returns true for #moderator?" do
-        expect(user.moderator?).to be_truthy
-      end
+    it "should be an invalid user due to blank name" do
+      expect(user_with_invalid_name).to_not be_valid
     end
 
-
-
-    describe "invalid user" do
-     let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
-     let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
-
-     it "should be an invalid user due to blank name" do
-       expect(user_with_invalid_name).to_not be_valid
-     end
-
-     it "should be an invalid user due to blank email" do
-       expect(user_with_invalid_email).to_not be_valid
-     end
-   end
-
-   describe "#favorite_for(post)" do
-    before do
-      topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
-      @post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
-    end
-
-    it "returns `nil` if the user has not favorited the post" do
-    expect(user.favorite_for(@post)).to be_nil
-    end
-
-    it "returns the appropriate favorite if it exists" do
-    favorite = user.favorites.where(post: @post).create
-    expect(user.favorite_for(@post)).to eq(favorite)
+    it "should be an invalid user due to blank email" do
+      expect(user_with_invalid_email).to_not be_valid
     end
   end
-end 
+end
